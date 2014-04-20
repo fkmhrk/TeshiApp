7f3e4c14e18b40e1bd14d271488f7e0091d65189
teshi04 <teshi04@gmail.com>
2014-04-20 19:50:52 +0900

    Upload images to s.tsur.jp

---
+++ app/src/main/java/jp/tsur/teshiapp/MainActivity.java
@@ -0,0 +1,100 @@
+package jp.tsur.teshiapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import utils.Utils;
+
+
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_GALLERY = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+        switch (requestCode) {
+            case REQUEST_GALLERY:
+                // URI を File に変換してアップロード
+                File file = Utils.getFileByUri(this, data.getData());
+                if (file == null) {
+                    Utils.showToast(this, getString(R.string.toast_failed));
+                    return;
+                }
+                new UploadTask().execute(file);
+                break;
+        }
+    }
+
+    @OnClick(R.id.upload_image_button)
+    void uploadImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_GALLERY);
+    }
+
+    private class UploadTask extends AsyncTask<File, Void, String> {
+        @Override
+        protected String doInBackground(File... files) {
+            // ポスト先のURLを指定
+            HttpPost httpPost = new HttpPost(getString(R.string.url_upload));
+            MultipartEntity multipartEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+            // ファイル名&パスを指定
+            FileBody fileBody = new FileBody(files[0]);
+
+            // KEYとファイルを指定
+            multipartEntity.addPart("imagedata", fileBody);
+            httpPost.setEntity(multipartEntity);
+
+            // レスポンス取得
+            HttpClient httpClient = new DefaultHttpClient();
+            httpPost.setEntity(multipartEntity);
+
+            try {
+                HttpResponse response = httpClient.execute(httpPost);
+                HttpEntity entity = response.getEntity();
+                return EntityUtils.toString(entity, "UTF-8");
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String str) {
+            if (str != null) {
+                Utils.copyToClipboard(MainActivity.this, str);
+                Utils.showToast(MainActivity.this, getString(R.string.toast_success_copy_to_clipboard));
+            } else {
+                Utils.showToast(MainActivity.this, getString(R.string.toast_failed));
+            }
+        }
+    }
+}
---
+++ app/src/main/java/utils/Utils.java
@@ -0,0 +1,58 @@
+package utils;
+
+
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import java.io.File;
+
+import static android.content.Context.CLIPBOARD_SERVICE;
+
+public class Utils {
+
+    public static void showToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+    }
+
+    public static void copyToClipboard(Context context, String str) {
+        //クリップボードに格納するItemを作成
+        ClipData.Item item = new ClipData.Item(str);
+
+        //MIMETYPEの作成
+        String[] mimeType = new String[1];
+        mimeType[0] = ClipDescription.MIMETYPE_TEXT_URILIST;
+
+        //クリップボードに格納するClipDataオブジェクトの作成
+        ClipData cd = new ClipData(new ClipDescription("text_data", mimeType), item);
+
+        //クリップボードにデータを格納
+        ClipboardManager cm = (ClipboardManager) context.getSystemService(CLIPBOARD_SERVICE);
+        cm.setPrimaryClip(cd);
+    }
+
+    public static File getFileByUri(Context context, Uri uri) {
+        ContentResolver cr = context.getContentResolver();
+        String[] columns = {MediaStore.Images.Media.DATA};
+        Cursor c = cr.query(uri, columns, null, null, null);
+        assert c != null;
+        c.moveToFirst();
+        String fileName = c.getString(0);
+
+        if (fileName == null) {
+            return null;
+        }
+        File path = new File(fileName);
+        if (!path.exists()) {
+            return null;
+        }
+
+        return path;
+    }
+}
